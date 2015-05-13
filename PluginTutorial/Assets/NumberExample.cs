883d947a17e5c4b80befd168eaaeee0739402524
Vivien Adnot <vivien.adnot@gmail.com>
2015-05-13 12:54:13 +0200

    initial commit

---
+++ PluginTutorial/Assets/NumberExample.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberExample : MonoBehaviour {
+	public Text uiText;
+	#if UNITY_ANDROID && !UNITY_EDITOR
+	AndroidJavaClass pluginTestJavaClass;
+	#endif
+
+	// Use this for initialization
+	void Start () {
+		GameObject textGameObject = GameObject.Find ("Text");
+		if (textGameObject == null) {
+			Debug.LogError("vivien: Can't find text component");
+		}
+
+		uiText = textGameObject.GetComponent<Text> ();
+		uiText.text = "start text ok";
+
+		#if UNITY_ANDROID && !UNITY_EDITOR
+		AndroidJNI.AttachCurrentThread ();
+		pluginTestJavaClass = new AndroidJavaClass ("com.test.plugintest.MainActivity");
+		#endif
+
+		Debug.Log ("Finish start without error");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_ANDROID && !UNITY_EDITOR
+		int number = pluginTestJavaClass.CallStatic<int> ("GetNumber");
+		uiText.text = "number: " + number;
+		#endif
+	}
+}
